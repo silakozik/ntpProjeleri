422b49c0953d95ac4d36a4d36da4c6c4d716eaf5
silakozik <silakozik2005@gmail.com>
2024-10-15 21:59:30 +0300

    adamAsmaca

---
+++ adamAsmaca/adamAsmaca/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace adamAsmaca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        /// <param name="disposing">yönetilen kaynaklar dispose edilmeliyse true; aksi halde false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        /// içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblKelime = new System.Windows.Forms.Label();
+            this.txtTahmin = new System.Windows.Forms.TextBox();
+            this.btnTahminEt = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.btnYeniOyun = new System.Windows.Forms.Button();
+            this.pictureBoxAdam = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAdam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblKelime
+            // 
+            this.lblKelime.AutoSize = true;
+            this.lblKelime.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKelime.Location = new System.Drawing.Point(555, 278);
+            this.lblKelime.Name = "lblKelime";
+            this.lblKelime.Size = new System.Drawing.Size(132, 25);
+            this.lblKelime.TabIndex = 0;
+            this.lblKelime.Text = "_ _ _ _ _ _ _";
+            this.lblKelime.Click += new System.EventHandler(this.lblKelime_Click);
+            // 
+            // txtTahmin
+            // 
+            this.txtTahmin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.txtTahmin.Location = new System.Drawing.Point(550, 62);
+            this.txtTahmin.Name = "txtTahmin";
+            this.txtTahmin.Size = new System.Drawing.Size(150, 30);
+            this.txtTahmin.TabIndex = 1;
+            this.txtTahmin.TextChanged += new System.EventHandler(this.txtTahmin_TextChanged);
+            // 
+            // btnTahminEt
+            // 
+            this.btnTahminEt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnTahminEt.Location = new System.Drawing.Point(550, 127);
+            this.btnTahminEt.Name = "btnTahminEt";
+            this.btnTahminEt.Size = new System.Drawing.Size(150, 57);
+            this.btnTahminEt.TabIndex = 2;
+            this.btnTahminEt.Text = "Tahmin Et";
+            this.btnTahminEt.UseVisualStyleBackColor = true;
+            this.btnTahminEt.Click += new System.EventHandler(this.btnTahminEt_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lblDurum.Location = new System.Drawing.Point(545, 347);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(158, 25);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "Yanlış Tahmin: 0";
+            // 
+            // btnYeniOyun
+            // 
+            this.btnYeniOyun.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnYeniOyun.Location = new System.Drawing.Point(550, 438);
+            this.btnYeniOyun.Name = "btnYeniOyun";
+            this.btnYeniOyun.Size = new System.Drawing.Size(150, 50);
+            this.btnYeniOyun.TabIndex = 5;
+            this.btnYeniOyun.Text = "Yeni Oyun";
+            this.btnYeniOyun.UseVisualStyleBackColor = true;
+            this.btnYeniOyun.Click += new System.EventHandler(this.btnYeniOyun_Click);
+            // 
+            // pictureBoxAdam
+            // 
+            this.pictureBoxAdam.Location = new System.Drawing.Point(58, 33);
+            this.pictureBoxAdam.Name = "pictureBoxAdam";
+            this.pictureBoxAdam.Size = new System.Drawing.Size(399, 476);
+            this.pictureBoxAdam.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxAdam.TabIndex = 6;
+            this.pictureBoxAdam.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 575);
+            this.Controls.Add(this.pictureBoxAdam);
+            this.Controls.Add(this.btnYeniOyun);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.btnTahminEt);
+            this.Controls.Add(this.txtTahmin);
+            this.Controls.Add(this.lblKelime);
+            this.Name = "Form1";
+            this.Text = "Adam Asmaca";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxAdam)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblKelime;
+        private System.Windows.Forms.TextBox txtTahmin;
+        private System.Windows.Forms.Button btnTahminEt;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Button btnYeniOyun;
+        private System.Windows.Forms.PictureBox pictureBoxAdam;
+    }
+}
---
+++ adamAsmaca/adamAsmaca/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace adamAsmaca
+{
+    public partial class Form1 : Form
+    {
+        string[] kelimeler = { "YILDIZLI GECE","MERHABA","DÜNYA","OYUN","PROGRAMLAMA",
+            "DENİZ","KURT","KELEBEK","KARAMEL","LABİRENT","BİLGİSAYAR","YALNIZLIK",
+            "SÜPERNOVA","ORKİDE","ATATÜRK","KAHVE","ÇİKOLATA","ÖTÜKEN","ŞİMŞEK","ATTİLA" };
+        string secilenKelime;
+        char[] tahminEdilenKelime;
+        int yanlisTahminSayisi = 0;
+        int maxYanlisSayisi = 10;
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            YeniOyun();
+        }
+
+        private void YeniOyun()
+        {
+            secilenKelime = kelimeler[random.Next(kelimeler.Length)];
+            tahminEdilenKelime = new string('_', secilenKelime.Length).ToCharArray();
+            lblKelime.Text = new string(tahminEdilenKelime);
+            yanlisTahminSayisi = 0;
+            lblDurum.Text = "Yanlış Tahmin Sayısı: 0";
+            pictureBoxAdam.Image = null;
+            btnTahminEt.Enabled = true;
+        }
+
+        private void btnTahminEt_Click(object sender, EventArgs e)
+        {
+           if(!string.IsNullOrWhiteSpace(txtTahmin.Text))
+            {
+                char tahmin = txtTahmin.Text.ToUpper()[0];
+                txtTahmin.Clear();
+
+                bool dogruTahmin = false;
+                for (int i = 0; i < secilenKelime.Length; i ++)
+                {
+                    if (secilenKelime[i] == tahmin)
+                    {
+                        tahminEdilenKelime[i] = tahmin;
+                        dogruTahmin = true;
+                    }
+                }
+                if(dogruTahmin)
+                {
+                    lblKelime.Text = new string(tahminEdilenKelime);
+                }
+                else
+                {
+                    yanlisTahminSayisi++;
+                    lblDurum.Text = "Yanlış Tahmin Sayısı:" + yanlisTahminSayisi;
+                    AdamResmiGuncelle();
+                }
+
+                if (yanlisTahminSayisi >= maxYanlisSayisi)
+                {
+                    MessageBox.Show("Kaybettiniz! Kelime:" + secilenKelime);
+                    btnTahminEt.Enabled = false;
+                }
+
+                if (new string(tahminEdilenKelime) == secilenKelime)
+                {
+                    MessageBox.Show("Tebrikler, kelimeyi buldunuz!");
+                    btnTahminEt.Enabled = false;
+                }
+            }
+        }
+
+        private void AdamResmiGuncelle()
+        {
+            if (yanlisTahminSayisi <= 10)
+            {
+                string resimYolu = $"Paint/adam{yanlisTahminSayisi}.png"; // 'Images' yerine 'Paint' yazıldı
+                try
+                {
+                    pictureBoxAdam.Image = Image.FromFile(resimYolu);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Resim bulunamadı: " + resimYolu);
+                }
+            }
+        }
+
+
+        private void btnYeniOyun_Click(object sender, EventArgs e)
+        {
+            YeniOyun();
+        }
+
+        private void txtTahmin_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblKelime_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
